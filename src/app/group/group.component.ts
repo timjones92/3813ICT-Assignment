bdf9db5c7321e488cc140121739362f75f71f886
timjones92 <timothy.jones6@griffithuni.edu.au>
2019-09-28 20:42:47 +1000

    Updated group component UI.

--- src/app/group/group.component.ts
+++ src/app/group/group.component.ts
@@ -25,13 +25,19 @@ export class GroupComponent implements OnInit {
   };
   authenticated: string;
   fileToUpload: any;
+  imgURL: any;
+  groupCount: number = 0;
   
   // Check if current user function
   readLocalStorageValue(key) {
     return localStorage.getItem(key);
   }
 
-  constructor(private userData: UserService, private groupData: GroupsService, private channelData: ChannelService) { }
+  constructor(
+    private userData: UserService, 
+    private groupData: GroupsService, 
+    private channelData: ChannelService
+    ) { }
 
   ngOnInit() {
     this.authenticated = this.readLocalStorageValue('username');
@@ -58,15 +64,29 @@ export class GroupComponent implements OnInit {
         for (let i = 0; i < this.users.length; i++) {
           if (this.users[i].username === this.localUsername) {
             this.currentUser = this.users[i];
+            if (this.userGroups.find(x => x.userID === this.currentUser._id)) {
+              this.groupCount = this.userGroups.find(x => x.userID === this.currentUser._id)
+              console.log("Current User Group count:", this.groupCount)
+            }
           }
         }
         console.log("Current User:", this.currentUser);
       }
     });
+
+    // Get all current users posts
+    
   }
 
   public onFileAdded(event) {
-    this.fileToUpload = event.target.files[0]
+    this.fileToUpload = event.target.files[0];
+    var reader = new FileReader();
+    if (this.fileToUpload !== undefined) {
+      reader.readAsDataURL(this.fileToUpload);
+      reader.onload = (_event) => {
+        this.imgURL = reader.result;
+      }
+    }
   }
   
   public onFileSubmit() {
